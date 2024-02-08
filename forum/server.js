67425f4ea9d99167cd0e318449f97f7f541c5a61
mandeukJeong <wjdalstj137@gmail.com>
2024-02-08 23:40:32 +0900

    part1 - 데이터 출력

--- forum/server.js
+++ forum/server.js
@@ -24,7 +24,8 @@ app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
 
-app.get('/news', (요청, 응답) => {
-    db.collection('post').insertOne({title: '어쩌구'})
-    // 응답.send('오늘 비옴')
-}) 
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result[0].title)
+    응답.send(result[0].title)
+})
