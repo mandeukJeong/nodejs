e479089cb79ae0aaac6757bea43de77f1fe0d669
mandeukJeong <wjdalstj137@gmail.com>
2024-02-08 23:21:18 +0900

    part1 - MongoDB 연결

--- forum/server.js
+++ forum/server.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
+const { PORT, MONGODB_URI } = process.env;
+
 const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, () => {
+const { MongoClient } = require('mongodb')
+
+let db;
+const url = MONGODB_URI
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(PORT, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
+})
+}).catch((err)=>{
+  console.log(err)
 })
 
 app.get('/', (요청, 응답) => {
@@ -12,13 +25,6 @@ app.get('/', (요청, 응답) => {
 })
 
 app.get('/news', (요청, 응답) => {
-    응답.send('오늘 비옴')
+    db.collection('post').insertOne({title: '어쩌구'})
+    // 응답.send('오늘 비옴')
 }) 
-
-app.get('/shop', (요청, 응답) => {
-    응답.send('쇼핑페이지입니다')
-})
-
-app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname + '/about.html')
-})
