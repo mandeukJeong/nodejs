1894284160dc1d07f3059496dc4a2e5094701f93
mandeukJeong <wjdalstj137@gmail.com>
2024-02-09 00:04:17 +0900

    part1 - ejs

--- forum/server.js
+++ forum/server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
 
 const { MongoClient } = require('mongodb')
 
@@ -26,6 +27,6 @@ app.get('/', (요청, 응답) => {
 
 app.get('/list', async (요청, 응답) => {
     let result = await db.collection('post').find().toArray()
-    console.log(result[0].title)
-    응답.send(result[0].title)
+    console.log(result[0].title) 
+    응답.render('list.ejs', {posts: result})
 })
